dad9f8c1002d7b423cb4a267d57d0a89b143c904
JaredAssen <jared.assen@ucalgary.ca>
2022-04-09 16:59:19 -0600

    yup

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomerService } from 'src/app/services/customer.service';
+import { Customer } from 'src/app/models/customer-model';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html',
+  template: 
+  `<h1 style="font-size: xx-large;"> LOGIN</h1>
+ <mat-card style = "background-color: rgb(255, 255, 255);">
+   <div class="Email">
+     <mat-form-field appearance="fill">
+       <mat-label>Enter your email</mat-label>
+       <input matInput placeholder="" value="" required name="email" #email="ngModel" [(ngModel)]="Customer.email">
+     </mat-form-field>
+   </div>
+ 
+   <div class="Password">
+     <mat-form-field appearance="fill">
+       <mat-label>Enter you password</mat-label>
+       <input matInput placeholder="" value="" required name="password" #password="ngModel" [(ngModel)]="Customer.password">
+     </mat-form-field>
+   </div>
+
+   <div class="submit">
+     <button style = "background-color: rgb(255, 255, 255);" mat-button (click)="validate()"> Submit</button>
+   </div>
+ </mat-card>
+
+ <div class="Admin Login">
+   <button mat-button routerLink="/adminlogin">Admin Login</button>
+ </div>`,
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private customerService : CustomerService) { }
+
+  Customer = new Customer();
+  currentCustomers: Customer[] = [];
 
   ngOnInit(): void {
+    this.getCustomers()
+      .subscribe((data: any) => {
+        this.currentCustomers = data as Customer[];
+      });
+  }
+
+  getCustomers() {
+    return this.customerService.getAllCustomers();
+  }
+
+  validate(){
+    for(let i = 0; i<this.currentCustomers.length;  i++){
+      if(this.Customer.password == this.currentCustomers[i].password && this.Customer.email == this.currentCustomers[i].email) return true;
+    }
+    return false;
   }
 
 }
